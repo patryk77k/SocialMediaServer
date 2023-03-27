accf22ae83acc0226a0e5c8126587ed624713a73
patryk77k <patryk77.k@gmail.com>
2023-03-28 00:26:39 +0200

    get and post created

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,24 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
+export const getPosts = async (req, res) => {
   try {
-    const postMessage = PostMessage.find();
-  } catch (error) {}
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
-export const createdPost = (req, res) => {
-  res.send("Post Creation");
+export const createdPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.staus(404).json({ message: error.message });
+  }
 };
